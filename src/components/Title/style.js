605624534848b749e371e6b0c6a6a6d67fa70e9c
TalithaRLiberato <rodriguestalitha59@gmail.com>
2024-04-12 18:08:21 -0300

    first commit

---
+++ src/components/Title/style.js
@@ -0,0 +1,17 @@
+//importação dos estilos do Reactive Native
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    boxTitle:{
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+    },
+    textTitle:{
+        color: "#FF0043",
+        fontSize: 24,
+        fontWeight: "bold",
+    }
+});
+
+export default styles
